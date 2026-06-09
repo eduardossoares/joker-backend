383e577b3440943d70939f03b3d95e29a7eb5338
eduardossoares <eduardo21.net@gmail.com>
2026-06-08 22:27:38 -0300

    chore: update scripts on package.json to includes run:build and start:dev/deploy + add host on server.ts and .env.development

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { buildApp } from "./app";
 
 const PORT = Number(process.env.PORT || 8080);
+const HOST = process.env.HOST || "0.0.0.0";
 
 const app = buildApp();
 
-app.listen({ port: PORT }, () => {
+app.listen({ port: PORT, host: HOST }, () => {
   console.log("server is running on port:", PORT);
 });
