8c6120635d15f3389f25c7dddbeca723db42c8c7
eduardossoares <eduardo21.net@gmail.com>
2026-06-10 22:05:48 -0300

    chore: reorganize api versions

--- src/routes/v1.routes.ts
+++ src/routes/v1.routes.ts
@@ -1,5 +1,5 @@
 import type { FastifyInstance } from "fastify";
-import statusRoutes from "src/modules/status/v1.status.routes";
+import statusRoutes from "src/modules/status/status.routes";
 
 export default async function v1Routes(app: FastifyInstance) {
   app.register(statusRoutes, { prefix: "/status" });
